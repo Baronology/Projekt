1fa1ec788e0ebe12b194a435b4e31b46f62422b6
Baronology <dvfalkenhausen@outlook.de>
2023-05-01 16:34:27 +0200

    adding products

---
+++ frontend/src/data.js
@@ -0,0 +1,17 @@
+// 
+const data = {
+    products: [
+        {
+            name: 'Nike Slim Shirt',
+            slug: 'nike-slim-shirt',
+            category: 'Shirts',
+            image: '/images/p1.jpg',
+            price: 120,
+            countinstock:§ 10,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product',
+        },
+    ],
+};
