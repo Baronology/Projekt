1436dbe3033b3ee011fac12bd53fd27c64bd4d1b
Baronology <dvfalkenhausen@outlook.de>
2023-05-09 16:08:44 +0200

    changes

--- README.md
+++
@@ -1,9 +0,0 @@
-# zum starten
-
-go to next.js ordner
-
-than tipe npm run dev
-and go to localhost:3000
-
-wenn es nicht geht dann npm install
-und nochmal npm run dev
---
+++ next.js_13.4/app/layout.jsx
@@ -0,0 +1,28 @@
+import '@styles/globals.css';
+
+import Nav from '@components/nav';
+// import Provider from '@components/provider';
+
+export const metadata = {
+  title: 'Ändrej_Dorian',
+  Description: "Ändrej_Dorian's personal website",
+};
+
+const RootLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+
+        <main className="app">
+          <Nav />
+          {children}
+        </main>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
